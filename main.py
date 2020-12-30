4f48490f94f0b63d9c645d23714d03530d6049d6
pranavthammana <61713704+pranavthammana@users.noreply.github.com>
2020-12-30 14:46:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,14 @@
 # # Press Shift+F10 to execute it or replace it with your code.
 # # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 #
-import csv
+
 import reverse_geocoder as rg
-import pprint
 from geopy.geocoders import Nominatim
-
-def to_list(file):
-    """Converts an Ascii XYZ gridded file into a list of lists with the 0th index being lattiude, 1st index
-    being longitude and 2nd index being band"""
-
-    end_list = []
-    with open(file, 'r') as csv_file:
-        file_reader = csv_reader(csv_file, delimiter=' ')
-        for row in file_reader:
-            end_list.append(list(row))
-    return end_list
+from shapely.geometry import Point
+from shapely.geometry import shape
+from shapely.prepared import prep
+import csv
+import Constants
 
 
 def round_list(l, decimal):
@@ -25,16 +18,9 @@ def round_list(l, decimal):
     *Changes input list*"""
 
     for i in l:
-         l[0] = round(l[0], decimal)
-         l[1] = round(l[1], decimal)
+        l[0] = round(l[0], decimal)
+        l[1] = round(l[1], decimal)
 
-
-def invert_coordinates(geo_coordinates):
-    """Returns the inverted coordinate pair in the input list to make it latitude, longitude
-    from longitude, latitude"""
-
-    fixed = list(geo_coordinates)[::-1]
-    return tuple(fixed)
 
 def Check_India(geo_coordinates):
     """Given latitude, longitude returns whether coordinate is in India or not"""
@@ -44,6 +30,7 @@ def Check_India(geo_coordinates):
     else:
         return False
 
+
 def Check_India2(geo_coordinates):
     geolocator = Nominatim(user_agent="geoapiExercises")
     Latitude = "25.594095"
@@ -51,13 +38,38 @@ def Check_India2(geo_coordinates):
 
     location = geolocator.reverse(Latitude + "," + Longitude)
     return location
-    #location = geolocator.reverse(str(geo_coordinates[1]) + "," + str(geo_coordinates[0]))
+    # location = geolocator.reverse(str(geo_coordinates[1]) + "," + str(geo_coordinates[0]))
+
+
+def Check_India_gud(geo_coordinates):
+    India = dict()
+    for feature in Constants.country_data["features"]:
+        if feature['properties']['ADMIN'] == "India":
+            geom = feature["geometry"]
+            India['India'] = prep(shape(geom))
+    point = Point(geo_coordinates[1], geo_coordinates[0])  # Latitude then Longitude
+    if India['India'].contains(point):
+        return True
+    return False
+
+
+def Check_State(geo_coordinates):
+    States = dict()
+    # noinspection PyUnresolvedReferences
+    for feature in Constants.state_data["features"]:
+        States[feature['properties']["NAME_1"]] = prep(shape(feature['geometry']))
+    point = Point(geo_coordinates[1], geo_coordinates[0])
+    for state in States.keys():
+        if States[state].contains(point):
+            return state
+
 
 def to_string(l):
     for i in range(len(l)):
         for e in range(len(l[i])):
             l[i][e] = str(l[i][e])
     return l
+
 
 def to_xyz(l):
     file = open(r"Poverty.xyz", "w")
@@ -67,5 +79,10 @@ def to_xyz(l):
 
 
 if __name__ == "__main__":
-    for i in range(100):
-        print(Check_India2((34, 34)))
+    csv_file = open('clipped_ispop.xyz')
+    file_reader = csv.DictReader(csv_file, delimiter=' ')
+    next_result = 2
+    while next_result != None:
+        x = input("")
+        next_result = next(file_reader)
+        print(next_result)
